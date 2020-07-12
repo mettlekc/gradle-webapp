5fbe280d10a87610f6a1eb85a57fb5051f374406
cheol <mettlekc@gmail.com>
2020-07-12 18:14:53 +0900

    Initial commit

---
+++ src/main/java/com/cheol/sample/controller/BaseController.java
@@ -0,0 +1,21 @@
+package com.cheol.sample.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class BaseController {
+
+    @Value("${zone}")
+    String zone;
+
+    @RequestMapping("/")
+    public String base() {
+        log.info("{}", zone);
+        return zone;
+    }
+
+}
